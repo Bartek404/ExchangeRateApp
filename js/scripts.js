6c6cab5eb6170e44b1004306f657f78e534421ce
Bartek <110336189+Bartek404@users.noreply.github.com>
2023-08-16 15:01:44 +0200

    Exchange Rate App based on API

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+const amountOne = document.querySelector('.amount-one')
+const amountTwo = document.querySelector('.amount-two')
+const currencyOne = document.querySelector('#currency-one')
+const currencyTwo = document.querySelector('#currency-two')
+const swapBtn = document.querySelector('.swap')
+const rateInfo = document.querySelector('.rate-info')
+const infoDate = document.querySelector('.date')
+
+const calculate = () => {
+	fetch(
+		`https://api.exchangerate.host/convert?from=${currencyOne.value}&to=${currencyTwo.value}&amount=${amountOne.value}&source=ecb`
+	)
+		.then(res => res.json())
+		.then(data => {
+			const from = data.query.from
+			const to = data.query.to
+			const rate = data.info.rate.toFixed(4)
+			const result = data.result.toFixed(2)
+			amountTwo.value = result
+			infoDate.textContent = data.date
+			rateInfo.textContent = `1 ${from} = ${rate} ${to}`
+		})
+		.catch(err => console.error(err))
+}
+
+const swap = () => {
+	const temp = currencyTwo.value
+	currencyTwo.value = currencyOne.value
+	currencyOne.value = temp
+	calculate()
+}
+
+currencyOne.addEventListener('change', calculate)
+currencyTwo.addEventListener('change', calculate)
+amountOne.addEventListener('input', calculate)
+swapBtn.addEventListener('click', swap)
+calculate()
